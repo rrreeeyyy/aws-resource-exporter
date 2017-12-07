30d7de8195d0a05d3b15f92cd1448ade6e612648
Ryota Yoshikawa <ryota-yoshikawa@cookpad.com>
2017-12-07 20:58:55 +0900

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from boto3 import client, resource
+from collections import Counter
+from itertools import groupby
+from prometheus_client import Gauge, generate_latest
+
+def handler(event, context):
+    instance_gauge = Gauge('aws_instance_count', 'AWS instance count by instance_type', ['instance_type'])
+    spot_instance_guage = Gauge('aws_spot_instance_count', 'AWS spot instance count by instance_type', ['instance_type'])
+    _aws_ec2_instance_count(instance_gauge)
+    _aws_ec2_spot_instance_requests_count(spot_instance_guage)
+
+    return generate_latest().decode('utf-8')
+
+def _aws_ec2_instance_count(gauge):
+    ec2 = resource('ec2')
+    instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+    counter = Counter([instance.instance_type for instance in instances])
+
+    for instance_type, count in counter.most_common():
+        gauge.labels(instance_type).inc(count)
+
+def _aws_ec2_spot_instance_requests_count(gauge):
+    ec2 = client('ec2')
+    instances = ec2.describe_spot_instance_requests(Filters=[{'Name': 'state', 'Values': ['active']}])['SpotInstanceRequests']
+    counter = Counter([instance['LaunchSpecification']['InstanceType'] for instance in instances])
+
+    for instance_type, count in counter.most_common():
+        gauge.labels(instance_type).inc(count)
+
+if __name__ == '__main__':
+    print(handler(None, None))
