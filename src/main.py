3fb929eddb5893edbd962b62c799fb43152e1666
Ryota Yoshikawa <ryota-yoshikawa@cookpad.com>
2017-12-12 17:19:18 +0900

    Install and follow flake8

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from boto3 import client, resource
 from collections import Counter
-from itertools import groupby
 from prometheus_client import Gauge, generate_latest
+
 
 def handler(event, context):
     instance_gauge = Gauge('aws_instance_count', 'AWS instance count by instance_type', ['instance_type'])
@@ -14,6 +14,7 @@ def handler(event, context):
     }
     return {'statusCode': 200, 'body': generate_latest().decode('utf-8'), 'headers': headers}
 
+
 def _aws_ec2_instance_count(gauge):
     ec2 = resource('ec2')
     instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
@@ -22,6 +23,7 @@ def _aws_ec2_instance_count(gauge):
     for instance_type, count in counter.most_common():
         gauge.labels(instance_type).inc(count)
 
+
 def _aws_ec2_spot_instance_requests_count(gauge):
     ec2 = client('ec2')
     instances = ec2.describe_spot_instance_requests(Filters=[{'Name': 'state', 'Values': ['active']}])['SpotInstanceRequests']
@@ -29,6 +31,7 @@ def _aws_ec2_spot_instance_requests_count(gauge):
 
     for instance_type, count in counter.most_common():
         gauge.labels(instance_type).inc(count)
+
 
 if __name__ == '__main__':
     print(handler(None, None))
