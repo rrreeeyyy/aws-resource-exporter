6d5fe1f8e54172b6c3521e67f0daec72b59b9dc7
Ryota Yoshikawa <ryota-yoshikawa@cookpad.com>
2017-12-14 15:24:20 +0900

    Use queryStringParameters['region'] for specify region name

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from boto3 import client, resource
+from boto3 import client
 from collections import Counter
 from prometheus_client import Gauge, generate_latest
 
@@ -6,8 +6,8 @@ from prometheus_client import Gauge, generate_latest
 def handler(event, context):
     instance_gauge = Gauge('aws_instance_count', 'AWS instance count by instance_type', ['instance_type'])
     spot_instance_guage = Gauge('aws_spot_instance_count', 'AWS spot instance count by instance_type', ['instance_type'])
-    _aws_ec2_instance_count(instance_gauge)
-    _aws_ec2_spot_instance_requests_count(spot_instance_guage)
+    _aws_ec2_instance_count(instance_gauge, event)
+    _aws_ec2_spot_instance_requests_count(spot_instance_guage, event)
 
     headers = {
         'Content-Type': 'text/plain'
@@ -15,17 +15,17 @@ def handler(event, context):
     return {'statusCode': 200, 'body': generate_latest().decode('utf-8'), 'headers': headers}
 
 
-def _aws_ec2_instance_count(gauge):
-    ec2 = resource('ec2')
-    instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-    counter = Counter([instance.instance_type for instance in instances])
+def _aws_ec2_instance_count(gauge, event):
+    ec2 = client('ec2', region_name=event['queryStringParameters'].get('region', 'ap-northeast-1'))
+    instances = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])['Reservations']
+    counter = Counter([instance['Instances'][0]['InstanceType'] for instance in instances])
 
     for instance_type, count in counter.most_common():
         gauge.labels(instance_type).inc(count)
 
 
-def _aws_ec2_spot_instance_requests_count(gauge):
-    ec2 = client('ec2')
+def _aws_ec2_spot_instance_requests_count(gauge, event):
+    ec2 = client('ec2', region_name=event['queryStringParameters'].get('region', 'ap-northeast-1'))
     instances = ec2.describe_spot_instance_requests(Filters=[{'Name': 'state', 'Values': ['active']}])['SpotInstanceRequests']
     counter = Counter([instance['LaunchSpecification']['InstanceType'] for instance in instances])
 
@@ -34,4 +34,8 @@ def _aws_ec2_spot_instance_requests_count(gauge):
 
 
 if __name__ == '__main__':
-    print(handler(None, None))
+    event = {
+        'queryStringParameters': {}
+    }
+    context = {}
+    print(handler(event, context))
